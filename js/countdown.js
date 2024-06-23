5be3b405289c37e4fd306278b2d4275a5822f106
劉軒羽 <130119566+ryukyucoding@users.noreply.github.com>
2024-06-23 22:06:02 +0800

    Update countdown.js

--- js/countdown.js
+++ js/countdown.js
@@ -1,52 +1,35 @@
-// var time = new Date();
+document.addEventListener("DOMContentLoaded", function() {
+    // 目标时间
+    var targetDate = new Date("2024-08-22T00:00:00").getTime();
 
-document.querySelector('.days').innerHTML = '0';
-document.querySelector('.hours').innerHTML = '0';
-document.querySelector('.minutes').innerHTML = '0';
-document.querySelector('.minutes').innerHTML = '0';
-document.querySelector('.seconds').innerHTML = '0';
-// time.setMonth(9);
-// time.setDate(23);
-// time.setHours(9);
-// time.setMinutes(0);
-// time.setSeconds(0);
-// var endTime = time.getTime();
+    // 更新倒计时的函数
+    function updateCountdown() {
+        var now = new Date().getTime();
+        var timeDifference = targetDate - now;
 
-// setInterval(function(){
-//     var time_2 = new Date();
-//     var nowTime = time_2.getTime();
-//     var offsetTime = (endTime - nowTime) / 1000;
-//     var sec = parseInt(offsetTime % 60);
-//     var min = parseInt((offsetTime / 60) % 60);
-//     var hr = parseInt((offsetTime / 60 / 60) % 24);
-//     var day = parseInt(offsetTime / 60 / 60 / 24);
+        // 计算天、小时、分钟和秒
+        var days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
 
-//     if(day < 10){
-//         document.querySelector('.days').innerHTML = '0'+ day +'';
-//     }
-//     else{
-//         document.querySelector('.days').innerHTML = ''+ day +'';
-//     }
+        // 更新 HTML 元素
+        document.querySelector('.days').textContent = days;
+        document.querySelector('.hours').textContent = hours;
+        document.querySelector('.minutes').textContent = minutes;
+        document.querySelector('.seconds').textContent = seconds;
 
-//     if(hr < 10){
-//         document.querySelector('.hours').innerHTML = '0'+ hr +'';
-//     }
-//     else{
-//         document.querySelector('.hours').innerHTML = ''+ hr +'';
-//     }
+        // 如果倒计时结束
+        if (timeDifference < 0) {
+            clearInterval(interval);
+            document.querySelector('.days').textContent = 0;
+            document.querySelector('.hours').textContent = 0;
+            document.querySelector('.minutes').textContent = 0;
+            document.querySelector('.seconds').textContent = 0;
+            // 你可以在这里添加额外的逻辑，比如显示消息等
+        }
+    }
 
-//     if(min < 10){
-//         document.querySelector('.minutes').innerHTML = '0'+ min +'';
-//     }
-//     else{
-//         document.querySelector('.minutes').innerHTML = ''+ min +'';
-//     }
-
-//     if(sec < 10){
-//         document.querySelector('.seconds').innerHTML = '0'+ sec +'';
-//     }
-//     else{
-//         document.querySelector('.seconds').innerHTML = ''+ sec +'';
-//     }
-
-// },1000);
+    // 每秒更新一次倒计时
+    var interval = setInterval(updateCountdown, 1000);
+});
